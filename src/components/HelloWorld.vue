8b359ea69150229b265eab57f9e0487222dea765
chenliang <chenliang@liandisys.com.cn>
2021-05-14 13:35:28 +0800

    init -rep

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- 内部组件 -->
+    <in-component00 v-bind:ic00tip="listObj00"></in-component00>
+    <!-- 外部组件 -->
+    <common-00 v-bind:data01="data01" static-data="static data">
+      <span slot="slot00" style="background-color: green"
+        >common00 插槽内容</span
+      >
+
+      <span slot="slot01" style="background-color: blue"
+        >common01 插槽内容</span
+      >
+    </common-00>
+  </div>
+</template>
+
+<script>
+import common00 from "../view/common/Common00";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      data00: "data00_val",
+      data01: "data01_val",
+      listObj00: [
+        { id: 0, val: "北京" },
+        { id: 1, val: "天津" },
+        { id: 2, val: "上海" },
+      ],
+    };
+  },
+  methods: {},
+  components: {
+    // ============================================================
+    // 内部组件
+    inComponent00: {
+      // 组件使用是峰驼转横杠 -> <in-component00></in-component00>
+      props: ["ic00tip"], // 对应v-bind 所绑定得key
+      template: "<li>来自内部组件 {{ ic00tip[1].val }}</li>",
+    },
+    // ============================================================
+    // 外部组件
+    common00: common00,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/view/common/Common00.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="box00">
+    <p>来自common00 component 的内容</p>
+
+    <div>
+      <span>插槽00 begin</span>
+      <slot name="slot00"></slot>
+      <span>插槽00 end</span>
+    </div>
+
+    <div>
+      <span>插槽01 begin</span>
+      <slot name="slot01"></slot>
+      <span>插槽01 end</span>
+    </div>
+
+    <div class="yellowBGC">
+      来自父组件的动态data： {{ data01 }}<br />来自父组件的静态data：{{
+        staticData
+      }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Common00",
+  data() {
+    return {};
+  },
+  computed: {},
+  props: {
+    data01: {
+      type: String, //指定传入的类型
+      //type 也可以是一个自定义构造器函数，使用 instanceof 检测。
+      default: "default_string", //这样可以指定默认的值
+    },
+    staticData: String,
+  },
+};
+</script>
+
+<style scoped>
+.box00 {
+  height: 30px;
+  background-color: red;
+}
+
+.yellowBGC {
+  background-color: yellow;
+}
+
+.greenBGC {
+  background-color: green;
+}
+</style>
